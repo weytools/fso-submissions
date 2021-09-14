82157eb6ea2b40225ca401172695a3686e8f741e
isaac <64103675+weytools@users.noreply.github.com>
2021-09-14 16:57:34 -0400

    1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,41 @@
+import { checkPropTypes } from 'prop-types'
 import React from 'react'
 
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <p>{props.partName} {props.partExercises}</p>
+  )
+}
+const Total = (props) => {
+  return(
+  <p><p>Number of exercises {props.totalNumber}</p></p>
   )
 }
 
 const App = () => {
-return (
-  <div>
-    <h1>Greetings</h1>
-    <Hello />
-    <Hello />
-    <Hello />
-    
-  </div>
-)}
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content partName={part1} partExercises={exercises1} />
+      <Content partName={part2} partExercises={exercises2} />
+      <Content partName={part3} partExercises={exercises3} />
+      <Total totalNumber={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
 
 export default App
