485bcf0c6513c524b6c74a7d661e1ec066959e96
Isaac <weytools@gmail.com>
2021-10-03 20:01:19 -0400

    1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -20,32 +20,35 @@ const Part = (props) =>
   <p>{props.partName} {props.partExercises}</p>
 )
 const Total = (props) => {
+  const totalNumber = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
   return(
-  <p>Number of exercises {props.totalNumber}</p>
+  <p>Number of exercises {totalNumber}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
   
-  const part1 = {
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[part1, part2, part3]}/>
-      <Total totalNumber={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
