a8ac4f20cc7ae44cdf6e3e41b77317c2689870d9
Isaac <weytools@gmail.com>
2022-01-17 10:12:24 -0500

    1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,81 +1,35 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-
-  const newFeedback = (feedback) =>
-  {
-    switch (feedback) {
-      case 'good':
-        setGood(good+1)        
-        break;
-      case 'neutral':
-        setNeutral(neutral+1)        
-        break;
-      case 'bad':
-        setBad(bad+1)        
-        break;
-      default:
-        break;
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const getRandomAnecdote = (currentIndex) => {
+    // generate random index
+    // do again if same as current
+    let newIndex = currentIndex;
+    while (newIndex == currentIndex) {
+      newIndex = Math.floor(Math.random() * anecdotes.length)
     }
-  }
-  const FeedbackButton = (props) =>{
-    return(
-      <button onClick={props.handleClick}>{props.label}</button>
-    )
+    // set state to new number
+    setSelected(newIndex)
   }
 
-  const StatsLine = (props) => {
-    return(
-    <tbody>
-      <tr>
-        <td>{props.label}</td>
-        <td>{props.data}</td>
-      </tr>
-    </tbody>
-    )
-  }
+  const AnecdoteButton = (props) => <button onClick={props.handleClick}>Random Anecdote</button>
 
-  const Statistics = (props) => {
-
-    const [good, neutral, bad] = props.data
-
-    if (good + neutral + bad == 0){
-      return (
-        <div>
-          <h2>Statistics</h2>
-          <h4>No feedback given</h4>
-        </div>
-      )
-    }
-
-
-    return (
-      <div>
-        <h2>Statistics</h2>
-        <table>
-          <StatsLine label={"Good"} data={good} />
-          <StatsLine label={"Neutral"} data={neutral} />
-          <StatsLine label={"Bad"} data={bad} />
-          <StatsLine label={"All"} data={good + neutral + bad} />
-          <StatsLine label={"Average"} data={(good - bad)/(good+bad)} />
-          <StatsLine label={"Positive"} data={(good/(good + neutral + bad))*100 + '%'} />
-        </table>
-      </div>
-    )
-  }
   return (
     <div>
-      <h2>Give Feedback</h2>
-      <FeedbackButton handleClick={()=>newFeedback('good')} label={'Good'} />
-      <FeedbackButton handleClick={()=>newFeedback('neutral')} label={'Neutral'} />
-      <FeedbackButton handleClick={()=>newFeedback('bad')} label={'Bad'} />
-      <hr/>
-      <Statistics data={[good, neutral, bad]}/>
+      {anecdotes[selected]}
+      <br/>
+      <AnecdoteButton handleClick={()=>getRandomAnecdote(selected)}/>
     </div>
   )
 }
