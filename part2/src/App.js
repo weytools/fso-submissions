a39bc0f38b9bc14d54482a3e27f97b0135d5c40c
isaac <64103675+weytools@users.noreply.github.com>
2022-02-27 12:08:01 -0500

    part2 reinit

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+var element2 = "wow"
+console.log('%c%s', 'color: #ff0000', element2)
+console.log(`%c${element2}`, 'color: #ff000050' )
 function App() {
   return (
     <div>
-    {console.log("test")}
+      <p>{element}</p>
     </div>
   );
 }
